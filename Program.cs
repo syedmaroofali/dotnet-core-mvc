e8dff93f22be3fe348a6b48c7904388724dfff71
relessawy <ramy_essawy@yahoo.com>
2017-08-18 03:58:51 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace WebApplication
 {
     public class Program
     {
-        //This version 0.3
+        
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
